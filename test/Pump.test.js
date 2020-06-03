71ccc25dd9b8cacee9ef636ad6d2db6d9e2b6cfa
Ravi Gairola <mallox@pyxzl.net>
2020-06-03 16:59:05 -0700

    Add todo Github action

--- test/Pump.test.js
+++ test/Pump.test.js
@@ -2,8 +2,14 @@ import Pump from '../src/Pump.js'
 import chai from 'chai'
 const expect = chai.expect;
 
+/**
+ * TODO: Put real tests here
+ * So far there are no tests anywhere and I'm busy writing configuration and scaffolding code.
+ * Once we're starting on the actual implementation this should be replaced with some real tests.
+ */
 describe('Pump', () => {
   it('should run a simple test to check qa setup', () => {
     expect(1).to.be.a('number');
+    expect(true).to.be.a('boolean');
   })
 })
